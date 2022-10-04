6643bc03c44b7e9fd5d701e1f9ea824f46eb75a7
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2022-10-04 14:41:00 +0200

    Completando el reto de Spiderman

---
+++ Reto Spiderman.py
@@ -0,0 +1,13 @@
+puntoA=0
+puntoB=0
+
+puntoA=(int)(input("¿En qué punto es la primera misión? (punto más cercano a 0)= "))
+puntoB=(int)(input("¿En qué punto es la segunda misión? (punto más lejano a 0)= "))
+
+if(puntoA>0 and puntoB>0):
+    print("La distancia recorrida es" , puntoB)
+
+if (puntoA<0 and puntoB>0 and abs(puntoA)<puntoB):
+    print("La distancia recorrida es", ((abs(puntoA*2) + puntoB)))
+
+if 
